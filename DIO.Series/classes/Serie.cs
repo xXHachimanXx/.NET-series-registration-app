1d338f699ca627200ea6b6cdb36dc80cc6318a4a
xXHachimanXx <globaldep90@gmail.com>
2021-03-19 17:13:45 -0300

    Adding the Genre enum.

--- DIO.Series/classes/Serie.cs
+++ DIO.Series/classes/Serie.cs
@@ -2,5 +2,9 @@ namespace DIO.Series
 {
     public class Serie : BaseEntity {
         // Atributes
+        private Genero genero {get; set;}
+        private string Titulo {get; set;}
+        private string Descricao {get; set;}
+        private int Ano {get; set;}
     }
 }
