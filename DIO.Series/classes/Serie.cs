e7db2309ab005f04e5f755beaf2d7e8f6a1d06af
xXHachimanXx <globaldep90@gmail.com>
2021-03-19 17:23:53 -0300

    Adding constructor and another methods to Serie class.

--- DIO.Series/classes/Serie.cs
+++ DIO.Series/classes/Serie.cs
@@ -1,10 +1,41 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : BaseEntity {
         // Atributes
-        private Genre genre {get; set;}
+        private Genre Genre {get; set;}
         private string Title {get; set;}
         private string Description {get; set;}
         private int Year {get; set;}
+
+        public Serie(int id, Genre genre, string title, string description, int year)
+		{
+			this.Id = id;
+			this.Genre = genre;
+			this.Title = title;
+			this.Description = description;
+			this.Year= year;
+		}
+
+        public override string ToString()
+		{
+            string retorno = "";
+            retorno += "Gênero: " + this.Genre + Environment.NewLine;
+            retorno += "Titulo: " + this.Title + Environment.NewLine;
+            retorno += "Descrição: " + this.Description + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Year + Environment.NewLine;
+            
+			return retorno;
+		}
+        public string returnTitle()
+		{
+			return this.Title;
+		}
+
+		public int returnId()
+		{
+			return this.Id;
+		}
     }
 }
